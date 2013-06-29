acc1e0397eaba6f8d2ef234f2b1068146b35a254
lulalala <mark@goodlife.tw>
2013-06-29 21:20:41 +0800

    ChinaTimes layout updated

--- lib/taiwanese_news_parser/parser/china_times.rb
+++ lib/taiwanese_news_parser/parser/china_times.rb
@@ -3,21 +3,28 @@ class TaiwaneseNewsParser::Parser::ChinaTimes < TaiwaneseNewsParser::Parser
     'chinatimes.com'
   end
 
+  def set_doc(url)
+    @raw = open(url).read
+    @doc = Nokogiri::HTML(@raw)
+  end
+
   #url = 'http://news.chinatimes.com/mainland/11050505/112013041400325.html'
   def parse
-    @article[:title] = @doc.at_css('.highlight').text
+    @article[:title] = @doc.at_css('header h1').text
 
-    @article[:company_name] = @doc.css('.bar-align-left>ul.inline-list>li')[1].text
+    @article[:company_name] = @doc.at_css('.reporter>a').text
     if @article[:company_name] == '新聞速報'
       @article[:company_name] = nil
     end
 
-    @article[:content] = @doc.css('#ctkeywordcontent').text
+    @article[:content] = @doc.css('.page_container article>p').text
 
     #@article[:web_published_at] = Time.parse(@doc.at_css('#story_update').text)
 
     @article[:reporter_name] = parse_reporter_name()
-    @article[:published_at] = Time.parse(@doc.css('.bar-align-left>ul.inline-list>li')[0].text)
+
+    t = @doc.css('.reporter time').text.match(/(\d*)年(\d*)月(\d*)日 (\d*):(\d*)/)
+    @article[:published_at] = Time.new(t[1],t[2],t[3],t[4],t[5])
 
     clean_up
 
@@ -25,11 +32,13 @@ class TaiwaneseNewsParser::Parser::ChinaTimes < TaiwaneseNewsParser::Parser
   end
 
   def parse_reporter_name
-    text = @doc.css('.bar-align-left>ul.inline-list>li.last').text
+    text = @doc.css('.reporter>text()').text
     if match = text.match(%r{記者(.+?)[/／╱／]})
       reporter_name = match[1]
     elsif match = text.match(%r{【(.+?)[/／╱／]})
       reporter_name = match[1]
+    else
+      reporter_name = text
     end
     reporter_name
   end
@@ -40,7 +49,7 @@ class TaiwaneseNewsParser::Parser::ChinaTimes < TaiwaneseNewsParser::Parser
   end
 
   def reproduced?
-    @doc.css('div.articlebox ul.inline-list li.ui').text.include?('中央社')
+    @doc.css('.reporter>a').text.include?('中央社')
   end
 
   def self.parse_url_id(url)
