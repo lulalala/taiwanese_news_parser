e1c3a4a3b47955dacc1ee000b8e4d06019eeb0a1
pa4373 <pa4373@gmail.com>
2014-11-13 18:39:43 +0800

    ltn-fix

--- lib/taiwanese_news_parser/parser/liberty_times_news.rb
+++ lib/taiwanese_news_parser/parser/liberty_times_news.rb
@@ -23,14 +23,13 @@ class TaiwaneseNewsParser::Parser::LibertyTimesNews < TaiwaneseNewsParser::Parse
     @article[:company_name] = parse_company_name
     @article[:content] = doc.css('#newstext p').text
 
-    time = doc.at_css('#newstext span').text[%r{\d{4}-\d{1,2}-\d{1,2}\W*\d{2}:\d{2}}]
-    if time.nil?
-      match = doc.at_css('#newstext span').text.match(%r{(\d{2}):(\d{2})})
-      now = Time.now
-      today = Date.today
-      @article[:published_at] = Time.new(today.year, today.month, today.day, match[1].to_i, match[2].to_i)
-    else
-      @article[:published_at] = Time.parse("#{time}:00")
+    doc.css('script').each do |script|
+      match = script.content.match(%r{newsTime\s=\s\'(\d+)\';$})
+      if not match.nil?
+        timestamp = match.captures[0].to_i
+        @article[:published_at] = Time.at(timestamp)
+        break
+      end
     end
 
     @article[:reporter_name] = parse_reporter_name()
